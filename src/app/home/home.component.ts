27a3f0f70d3a127826eef1f069aa3597480ea7da
Surya0191 <ssuryateja804@gmail.com>
2025-02-06 17:30:07 -0600

    Implemented ForkJoin

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -14,7 +14,7 @@ export class HomeComponent {
   topics = [
     { title: 'Signals', description: 'Signals in Angular are a reactive state management feature that automatically updates the UI when the state changes, simplifying reactivity without the need for subscriptions.', route: '/signals' },
     { title: 'Custom Directives', description: 'Custom directives in Angular are used to extend HTML functionality by adding custom behavior to elements, components, or attributes.', route: '/directives' },
-    { title: 'Fork Join', description: 'Learn about Fork Join', route: '/forkjoin' },
+    { title: 'Fork Join', description: 'ForkJoin in Angular is used to execute multiple observables in parallel and emits their combined final values only when all observables complete.', route: '/forkjoin' },
     { title: 'DOM Sanitizer', description: 'Learn about Dom Sanitizer', route: '/sanitizer' }
   ];
 }
--- src/app/topics/forkjoin/forkjoin.component.ts
+++ src/app/topics/forkjoin/forkjoin.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-forkjoin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './forkjoin.component.html',
-  styleUrl: './forkjoin.component.scss'
+  styleUrl: './forkjoin.component.scss',
 })
-export class ForkjoinComponent {
+export class ForkjoinComponent implements OnInit {
+  isForkJoinEnabled = false;
+  schoolsCount = 0;
+  hospitalsCount = 0;
+  banksCount = 0;
+  isWidgetOneSpinner = false;
+  isWidgetTwoSpinner = false;
+  isWidgetThreeSpinner = false;
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.isWidgetOneSpinner = true;
+    this.isWidgetTwoSpinner = true;
+    this.isWidgetThreeSpinner = true;
+    if (this.isForkJoinEnabled) {
+      this.getAllData();
+    } else {
+      this.getData();
+    }
+  }
+
+  refreshData() {
+    this.schoolsCount = 0;
+    this.hospitalsCount = 0;
+    this.banksCount = 0;
+    this.ngOnInit();
+  }
+
+  toggleForkJoin(event: Event) {
+    this.isForkJoinEnabled = (event.target as HTMLInputElement).checked;
+  }
+
+  getData() {
+    this.dataService.getWidgetOneData().subscribe((data: any) => {
+      this.schoolsCount = data;
+      this.isWidgetOneSpinner = false;
+    });
+    this.dataService.getWidgetTwoData().subscribe((data: any) => {
+      this.hospitalsCount = data;
+      this.isWidgetTwoSpinner = false;
+    });
+    this.dataService.getWidgetThreeData().subscribe((data: any) => {
+      this.banksCount = data;
+      this.isWidgetThreeSpinner = false;
+    });
+  }
+
+  getAllData() {
+    this.dataService.getAllData().subscribe((res) => {
+      this.schoolsCount = res[0];
+      this.hospitalsCount = res[1];
+      this.banksCount = res[2];
+      this.isWidgetOneSpinner = false;
+      this.isWidgetTwoSpinner = false;
+      this.isWidgetThreeSpinner = false;
+    });
+  }
 }
