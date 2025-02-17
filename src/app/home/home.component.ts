07373bc267e3db9e3a4b9b4576a7e7b3bfe8004a
Surya0191 <ssuryateja804@gmail.com>
2025-02-17 16:40:34 -0600

    Added Dom Sanitizer.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common'; // Import CommonModule for ngFor
+import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatCardModule], // Add CommonModule here
+  imports: [CommonModule, RouterModule, MatCardModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
@@ -15,6 +15,6 @@ export class HomeComponent {
     { title: 'Signals', description: 'Signals in Angular are a reactive state management feature that automatically updates the UI when the state changes, simplifying reactivity without the need for subscriptions.', route: '/signals' },
     { title: 'Custom Directives', description: 'Custom directives in Angular are used to extend HTML functionality by adding custom behavior to elements, components, or attributes.', route: '/directives' },
     { title: 'Fork Join', description: 'ForkJoin in Angular is used to execute multiple observables in parallel and emits their combined final values only when all observables complete.', route: '/forkjoin' },
-    { title: 'DOM Sanitizer', description: 'Learn about Dom Sanitizer', route: '/sanitizer' }
+    { title: 'DOM Sanitizer', description: 'DOM Sanitizer in Angular helps prevent security risks by **cleaning and sanitizing untrusted HTML, URLs, styles, and scripts to protect against XSS attacks.', route: '/sanitizer' }
   ];
 }
--- src/app/topics/dom-sanitizer/dom-sanitizer.component.ts
+++ src/app/topics/dom-sanitizer/dom-sanitizer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-dom-sanitizer',
-  standalone: true,
-  imports: [],
   templateUrl: './dom-sanitizer.component.html',
-  styleUrl: './dom-sanitizer.component.scss'
+  standalone:true,
+  styleUrls: ['./dom-sanitizer.component.scss']
 })
 export class DomSanitizerComponent {
+  unsafeHtml: string;
+  sanitizedHtml: SafeHtml;
 
+  constructor(private sanitizer: DomSanitizer) {
+    this.unsafeHtml = `<p style="color: red;">Unsafe HTML with <script>alert('Hacked!')</script></p>`;
+    this.sanitizedHtml = this.sanitizer.bypassSecurityTrustHtml(this.unsafeHtml);
+  }
 }
